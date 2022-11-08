1017423d87ee7cfc470ec44121e1edbbb1edb641
jasperdifran <68628968+jasperdifran@users.noreply.github.com>
2022-11-08 15:41:29 +1100

    Fix formatting, align musl with sel4

--- arch/aarch64/syscall_arch.h
+++ arch/aarch64/syscall_arch.h
@@ -1,74 +1,43 @@
 #define __SYSCALL_LL_E(x) (x)
 #define __SYSCALL_LL_O(x) (x)
 
-#define __asm_syscall(...) do { \
-	__asm__ __volatile__ ( "svc 0" \
-	: "=r"(x0) : __VA_ARGS__ : "memory", "cc"); \
-	return x0; \
-	} while (0)
+extern unsigned long __sysinfo;
+
+#define CALL_SYSINFO(n, ...) ((long (*)(long, ...))__sysinfo)(n, ##__VA_ARGS__)
 
 static inline long __syscall0(long n)
 {
-	register long x8 __asm__("x8") = n;
-	register long x0 __asm__("x0");
-	__asm_syscall("r"(x8));
+    return CALL_SYSINFO(n);
 }
 
-static inline long __syscall1(long n, long a)
+static inline long __syscall1(long n, long a1)
 {
-	register long x8 __asm__("x8") = n;
-	register long x0 __asm__("x0") = a;
-	__asm_syscall("r"(x8), "0"(x0));
+    return CALL_SYSINFO(n, a1);
 }
 
-static inline long __syscall2(long n, long a, long b)
+static inline long __syscall2(long n, long a1, long a2)
 {
-	register long x8 __asm__("x8") = n;
-	register long x0 __asm__("x0") = a;
-	register long x1 __asm__("x1") = b;
-	__asm_syscall("r"(x8), "0"(x0), "r"(x1));
+    return CALL_SYSINFO(n, a1, a2);
 }
 
-static inline long __syscall3(long n, long a, long b, long c)
+static inline long __syscall3(long n, long a1, long a2, long a3)
 {
-	register long x8 __asm__("x8") = n;
-	register long x0 __asm__("x0") = a;
-	register long x1 __asm__("x1") = b;
-	register long x2 __asm__("x2") = c;
-	__asm_syscall("r"(x8), "0"(x0), "r"(x1), "r"(x2));
+    return CALL_SYSINFO(n, a1, a2, a3);
 }
 
-static inline long __syscall4(long n, long a, long b, long c, long d)
+static inline long __syscall4(long n, long a1, long a2, long a3, long a4)
 {
-	register long x8 __asm__("x8") = n;
-	register long x0 __asm__("x0") = a;
-	register long x1 __asm__("x1") = b;
-	register long x2 __asm__("x2") = c;
-	register long x3 __asm__("x3") = d;
-	__asm_syscall("r"(x8), "0"(x0), "r"(x1), "r"(x2), "r"(x3));
+    return CALL_SYSINFO(n, a1, a2, a3, a4);
 }
 
-static inline long __syscall5(long n, long a, long b, long c, long d, long e)
+static inline long __syscall5(long n, long a1, long a2, long a3, long a4, long a5)
 {
-	register long x8 __asm__("x8") = n;
-	register long x0 __asm__("x0") = a;
-	register long x1 __asm__("x1") = b;
-	register long x2 __asm__("x2") = c;
-	register long x3 __asm__("x3") = d;
-	register long x4 __asm__("x4") = e;
-	__asm_syscall("r"(x8), "0"(x0), "r"(x1), "r"(x2), "r"(x3), "r"(x4));
+    return CALL_SYSINFO(n, a1, a2, a3, a4, a5);
 }
 
-static inline long __syscall6(long n, long a, long b, long c, long d, long e, long f)
+static inline long __syscall6(long n, long a1, long a2, long a3, long a4, long a5, long a6)
 {
-	register long x8 __asm__("x8") = n;
-	register long x0 __asm__("x0") = a;
-	register long x1 __asm__("x1") = b;
-	register long x2 __asm__("x2") = c;
-	register long x3 __asm__("x3") = d;
-	register long x4 __asm__("x4") = e;
-	register long x5 __asm__("x5") = f;
-	__asm_syscall("r"(x8), "0"(x0), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5));
+    return CALL_SYSINFO(n, a1, a2, a3, a4, a5, a6);
 }
 
 #define VDSO_USEFUL
