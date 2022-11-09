7776747a1073af2f0740139eecaa738e751edc10
jasperdifran <68628968+jasperdifran@users.noreply.github.com>
2022-11-09 15:16:42 +1100

    Add working crt0

--- crt/aarch64/crt0.s
+++ crt/aarch64/crt0.s
@@ -1,5 +1,3 @@
-/* Found in AOS starter code in `aos-base/projects/sel4runtime/crt/sel4_arch/aarch64/ */
-
 /*
  * Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
  *
@@ -12,9 +10,8 @@ _start:
 	mov lr, #0
 
 	mov x0, sp
-	bl __sel4_start_c
+	bl _start_c
 
 	/* should not return */
 1:
 	b 1b
-
